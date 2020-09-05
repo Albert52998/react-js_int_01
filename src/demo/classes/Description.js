358fe3d2b21f23a761896c10cfc570a594236e44
Albert52998 <55946155+Albert52998@users.noreply.github.com>
2020-09-05 19:56:52 +0400

    Update Description.js

--- src/demo/classes/Description.js
+++ src/demo/classes/Description.js
@@ -1,10 +1,6 @@
 import React from 'react';
 
 export default class Description extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <p>{this.props.description}</p>
